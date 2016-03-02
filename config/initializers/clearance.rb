4090816f625d86a58b547fc60f6d913e98e2d57a
hww <836967373@qq.com>
2016-03-02 22:59:20 +0800

    custom clearance view

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,3 +1,7 @@
+Clearance::PasswordsController.layout 'layouts/clearance'
+Clearance::SessionsController.layout 'layouts/clearance'
+Clearance::UsersController.layout 'layouts/clearance'
+
 Clearance.configure do |config|
   config.mailer_sender = "reply@example.com"
 end
