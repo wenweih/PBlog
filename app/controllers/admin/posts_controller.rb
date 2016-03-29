06ba496efa9bc8f035e5059dfadd56e283047aae
hww <836967373@qq.com>
2016-03-29 23:41:31 +0800

    add tag for post

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -9,6 +9,7 @@ module Admin
     # end
     def update
       if requested_resource.update(resource_params)
+        requested_resource.tag_list.add(params[:tag_list]) if params[:tag_list]
         redirect_to(
           admin_posts_path,
           notice: "#{requested_resource.title}更新成功",
@@ -20,6 +21,20 @@ module Admin
       end
     end
 
+    def create
+      resource = resource_class.new(resource_params)
+      if resource.save
+        requested_resource.tag_list.add(params[:tag_list]) if params[:tag_list]
+        redirect_to(
+          admin_posts_path,
+          notice: translate_with_resource("create.success"),
+        )
+      else
+        render :new, locals: {
+          page: Administrate::Page::Form.new(dashboard, resource),
+        }
+     end
+   end
     # Define a custom finder by overriding the `find_resource` method:
     # def find_resource(param)
     #   User.find_by!(slug: param)
@@ -27,5 +42,10 @@ module Admin
 
     # See https://administrate-docs.herokuapp.com/customizing_controller_actions
     # for more information
+
+    def resource_params
+      params.require(:post).permit(:title, :content, :tag_list)
+    end
+
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, presence: true
   belongs_to :user
+  acts_as_taggable_on :tags
 end
