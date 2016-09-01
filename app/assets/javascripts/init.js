0afedade3fdaeb3054cf7633240a90664024efad
hww <836967373@qq.com>
2016-09-02 02:07:01 +0800

    listen click event and call corresponding handle function data-behavior value

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -5,12 +5,15 @@
 //= require jquery.timeago.zh-CN
 //= require js-routes
 //= require typed
+//=  require highlight
 window.App || (window.App = {});
 App = {
   init: function(){
     (function(){
       App.siteBootUp();
       App.turbolinksCache();
+      App.sitePageInit();
+      App.siteEvents();
     })();
   },
   siteBootUp: function(){
@@ -18,11 +21,19 @@ App = {
     App.initUI.initTimeago();
     App.initUI.initTyped();
     App.initUI.initSemanticUITool();
+    App.initUI.initHighLight();
     return;
+  },
+  sitePageInit: function(){
+    App.initPage.home();
+  },
+  siteEvents: function(){
+    App.bind.events();
   },
   turbolinksCache: function(){
     $(document).on('turbolinks:before-cache', function(){
       App.beforeCache.typedCache();
+      App.beforeCache.formCache();
     });
     return;
   },
@@ -30,6 +41,20 @@ App = {
   initUI: {
     initHeadroom: function(){
       $(".headeroom").headroom();
+      $(document).on('scroll', function(){
+        if ($(this).scrollTop() == 0){
+          $('.headeroom').addClass('header-transparent');
+        }else {
+          $('.headeroom').removeClass('header-transparent');
+          $(".headeroom").css({
+            'background':'#fff',
+            'border-bottom':'1px solid #DDD',
+          });
+          $(".headeroom").find("a").css({
+            color: '#66676e'
+          });
+        }
+      });
     },
     initTimeago: function(){
       $("time.timeago").timeago();
@@ -51,14 +76,49 @@ App = {
     initSemanticUITool: function(){
       $('.ui.sidebar').sidebar('attach events', '.toc.item');
       $('.sub_menu').sticky({ context: '.post-index' });
+    },
+    initHighLight: function(){
+      $('pre code').each(function(i, block) {
+        // http://stackoverflow.com/questions/31753617/how-can-i-remove-leading-whitespace-in-my-pre-code-block-without-removing-in
+        var html = $(this).html();
+        var pattern = html.match(/\s*\n[\t\s]*/);
+        $(this).html(html.replace(new RegExp(pattern, "g"),'\n'));
+
+        hljs.highlightBlock(block);
+      });
     }
   },
-
+  initPage: {
+    home: function(){
+      if($('.home.index').length > 0){
+        App.Home.messageForm();
+      }
+    }
+  },
   beforeCache:  {
     typedCache: function(){
       $('.typed-cursor').remove();
+    },
+    formCache: function(){
+      $('.contact_form').form('clear');
     }
   },
+  bind: {
+    events: function(){
+      $(document).on('click', function(e){
+        var behavior = $(e.target).attr("data-behavior");
+        if ( behavior !== undefined ) {
+          eval("App.events." + behavior + "();");
+        }
+      })
+    }
+  },
+  events: {
+    homeScrollNext: function(){
+      console.log("run...");
+      $("html,body").animate({ scrollTop: $(window).height()}, 800);
+    }
+  }
 };
 
 $(document).on('turbolinks:load', function(){
