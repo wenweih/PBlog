1ff98e91ff4d8f7d006aec975e2b7f1082147509
hww <836967373@qq.com>
2016-12-21 15:47:24 +0800

    fixed post tags and friendly_id bug

--- app/controllers/admin/blog_posts_controller.rb
+++ app/controllers/admin/blog_posts_controller.rb
@@ -9,8 +9,13 @@ module Admin
     # end
     def update
       if requested_resource.update(resource_params)
-        requested_resource.update_attribute(params[:friend_url]) if requested_resource.friend_url_changed?
-        requested_resource.tag_list.add(params[:tag_list]) if params[:tag_list]
+        params_tags = params[:blog_post][:tag_list]
+        db_tags = requested_resource.tag_list
+        add_tags = params_tags - db_tags
+        remove_tag = db_tags - params_tags
+        requested_resource.tag_list.add(add_tags)
+        requested_resource.tag_list.remove(remove_tag)
+        requested_resource.update_attributes!(friend_url: params[:blog_post][:friend_url], slug: nil)
         redirect_to(
           admin_posts_path,
           notice: "#{requested_resource.title}更新成功",
@@ -21,7 +26,7 @@ module Admin
         }
       end
     end
-    
+
     def create
       resource = resource_class.new(resource_params)
       if resource.save
--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,6 @@ module MyPage
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.autoload_paths << Rails.root.join('lib')
     config.active_record.raise_in_transactional_callbacks = true
-    config.lograge.enabled = true
+    config.lograge.enabled = false
   end
 end
