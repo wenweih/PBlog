c6aab09c7de4c40cd5ad7d96244b61c5c979503c
hww <836967373@qq.com>
2017-01-27 15:58:25 +0800

    add tag list to post index page

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -8,6 +8,7 @@ class PostController < ApplicationController
     @posts = params[:category].present? ? @posts.joins(:category).where('categories.id' => params[:category]) : @posts
     @posts = @posts.page(params[:page]).includes(:comments)
     @posts = params[:blog_type].present? ? @posts.tagged_with(params[:blog_type]) : @posts
+    @tags = ActsAsTaggableOn::Tag.order(taggings_count: :desc)
   end
 
   def show
