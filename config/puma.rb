cca3258c1da232422b3a9d8c577504074fa5e41b
Wenwei Huang <mr.huangwenwei@gmail.com>
2018-01-27 09:10:56 +0000

    server

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,17 @@
+app_root = '/home/hww/my_page'
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
+environment ENV.fetch("RAILS_ENV") { "development"  }
+pidfile "#{app_root}/tmp/pids/puma.pid"
+state_path "#{app_root}/tmp/pids/puma.state"
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
+daemonize true
+stdout_redirect "#{app_root}/log/stdout", "#{app_root}/log/stderr"
 
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+preload_app!
+
 on_worker_boot do
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 end
